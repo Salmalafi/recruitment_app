3579dc37ad8c747a04d08bebede4ce76c7294927
salma <lafiselma@gmail.com>
2024-07-04 22:29:03 +0100

    sign in & signup

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,7 +4,7 @@ import recrutement from '../assets/Resume-amico.png';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios'; 
 
-const SignUp = () => {
+const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errors, setErrors] = useState({});
@@ -44,10 +44,9 @@ const SignUp = () => {
           password,
         });
 
-        const { token } = response.data;
+        const { access_token } = response.data;
 
-        localStorage.setItem('token', token);
-
+        localStorage.setItem('token', access_token);
       
         navigate('/profile'); 
       } catch (error) {
@@ -70,7 +69,7 @@ const SignUp = () => {
           </div>
 
           <form onSubmit={handleSignUp} className="bg-white rounded-xl px-6 py-8 space-y-6 max-w-md md:ml-auto w-full">
-            <h3 className="text-3xl font-extrabold mb-12">Sign Up</h3>
+            <h3 className="text-3xl font-extrabold mb-12">Sign in</h3>
 
             <div className="mb-4">
               <label htmlFor="email" className="block text-sm font-medium text-gray-700">
@@ -116,18 +115,43 @@ const SignUp = () => {
 
             <div>
               <button type="submit" className="w-full shadow-xl py-3 px-6 text-sm font-semibold rounded-md text-white bg-gray-800 hover:bg-[#222] focus:outline-none">
-                Sign Up
+               Login
               </button>
             </div>
 
             <p className="my-6 text-sm text-gray-400 text-center">or continue with</p>
-
             <div className="space-x-6 flex justify-center">
+           
               <button type="button" className="border-none outline-none">
-                {/* Your SVG icon */}
+              <svg xmlns="http://www.w3.org/2000/svg" width="30px" class="inline" viewBox="0 0 512 512">
+                <path fill="#fbbd00"
+                  d="M120 256c0-25.367 6.989-49.13 19.131-69.477v-86.308H52.823C18.568 144.703 0 198.922 0 256s18.568 111.297 52.823 155.785h86.308v-86.308C126.989 305.13 120 281.367 120 256z"
+                  data-original="#fbbd00" />
+                <path fill="#0f9d58"
+                  d="m256 392-60 60 60 60c57.079 0 111.297-18.568 155.785-52.823v-86.216h-86.216C305.044 385.147 281.181 392 256 392z"
+                  data-original="#0f9d58" />
+                <path fill="#31aa52"
+                  d="m139.131 325.477-86.308 86.308a260.085 260.085 0 0 0 22.158 25.235C123.333 485.371 187.62 512 256 512V392c-49.624 0-93.117-26.72-116.869-66.523z"
+                  data-original="#31aa52" />
+                <path fill="#3c79e6"
+                  d="M512 256a258.24 258.24 0 0 0-4.192-46.377l-2.251-12.299H256v120h121.452a135.385 135.385 0 0 1-51.884 55.638l86.216 86.216a260.085 260.085 0 0 0 25.235-22.158C485.371 388.667 512 324.38 512 256z"
+                  data-original="#3c79e6" />
+                <path fill="#cf2d48"
+                  d="m352.167 159.833 10.606 10.606 84.853-84.852-10.606-10.606C388.668 26.629 324.381 0 256 0l-60 60 60 60c36.326 0 70.479 14.146 96.167 39.833z"
+                  data-original="#cf2d48" />
+                <path fill="#eb4132"
+                  d="M256 120V0C187.62 0 123.333 26.629 74.98 74.98a259.849 259.849 0 0 0-22.158 25.235l86.308 86.308C162.883 146.72 206.376 120 256 120z"
+                  data-original="#eb4132" />
+              </svg>
               </button>
-              <button type="button" className="border-none outline-none">
-                {/* Your SVG icon */}
+              <button type="button"
+      className="border-none outline-none">
+              <svg width="48px" height="48px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <title>Linkedin</title>
+    <g id="Icon/Social/linkedin-color" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <path d="M20.9716667,33.5527338 L25.001,33.5527338 L25.001,27.1328007 C25.001,25.439485 25.3213333,23.7988354 27.4206667,23.7988354 C29.491,23.7988354 29.517,25.7351486 29.517,27.2404662 L29.517,33.5527338 L33.5506667,33.5527338 L33.5506667,26.4341413 C33.5506667,22.9381777 32.796,20.2505391 28.711,20.2505391 C26.7483333,20.2505391 25.432,21.3265278 24.8943333,22.3471839 L24.839,22.3471839 L24.839,20.5725357 L20.9716667,20.5725357 L20.9716667,33.5527338 Z M16.423,14.1202696 C15.1273333,14.1202696 14.0823333,15.1682587 14.0823333,16.4595785 C14.0823333,17.7508984 15.1273333,18.7992208 16.423,18.7992208 C17.7133333,18.7992208 18.761,17.7508984 18.761,16.4595785 C18.761,15.1682587 17.7133333,14.1202696 16.423,14.1202696 L16.423,14.1202696 Z M14.4026667,33.5527338 L18.4406667,33.5527338 L18.4406667,20.5725357 L14.4026667,20.5725357 L14.4026667,33.5527338 Z M9.76633333,40 C8.79033333,40 8,39.2090082 8,38.2336851 L8,9.76631493 C8,8.79065843 8.79033333,8 9.76633333,8 L38.234,8 C39.2093333,8 40,8.79065843 40,9.76631493 L40,38.2336851 C40,39.2090082 39.2093333,40 38.234,40 L9.76633333,40 Z" id="Shape" fill="#007BB5"></path>
+    </g>
+</svg>
               </button>
             </div>
           </form>
@@ -137,5 +161,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
-
+export default Login; 
